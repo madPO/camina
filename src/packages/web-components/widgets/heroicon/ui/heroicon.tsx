971007bbd4580cae9a787c237fcab2adca7c9a76
madPO <madpo.s.a@gmail.com>
2023-03-05 16:42:15 +0300

    feat(web-component): добавлены стили в компонент иконки

--- src/packages/web-components/widgets/heroicon/ui/heroicon.tsx
+++ src/packages/web-components/widgets/heroicon/ui/heroicon.tsx
@@ -2,12 +2,16 @@ import { defineWebComponent } from '../../../shared/webComponents'
 import { Heroicon, defineHeroiconAttributes } from '@camina/core'
 import React, { JSX } from 'jsx-dom'
 import type { heroiconAttribute } from '@camina/core/widgets/heroicon/model/heroiconTypes'
+import css from '@camina/core/style/heroicon.css?raw'
 
 defineWebComponent('camina-heroicon', function ({ defineAttributes, onRender }) {
   defineAttributes(defineHeroiconAttributes())
 
   const component = (props: heroiconAttribute): JSX.Element =>
     <>
+      <style>
+        {css}
+      </style>
       <Heroicon {...props} />
     </>
 
